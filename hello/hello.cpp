18fb382cd306d3e7178068a611c11d28acef8388
Roger Zanoni <roger.zanoni@openbossa.org>
2015-02-27 14:30:27 -0400

    Add a class and a function to hello.cpp to test the llvm pass

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
+class Hello {
+public:
+    Hello() {}
+
+    void hello() {
+        printf("hello world from hello class\n");
+    }
+};
+
+void hello() {
+    printf("hello world from hello class\n");
+}
+
 int main() {
     printf("hello world\n");
+
+    Hello helloobj;
+    helloobj.hello();
+
+    hello();
+
     return 0;
 }
